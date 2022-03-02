3e455461bf2123087fcefd479fcd615af68e79d8
amaniq88 <amani.musallamq88@gmail.com>
2022-03-02 21:31:24 +0300

    function added

--- math_series/series.py
+++ math_series/series.py
@@ -1,4 +1,30 @@
+def FibRecursion():  
+    if n == 0:  
+       return 0  
+    elif n== 1 : 
+       return 1
+    else:
+       return(FibRecursion(n-1) + FibRecursion(n-2)) 
 
+
+
+def lucas():
+    if n == 0:  
+       return 2  
+    elif n== 1 : 
+       return 1
+    else:
+       return(lucas(n-1) + lucas(n-2))  
+
+def sum_series(n, num= 0, nn= 1):
+    if n == 0: 
+       print(num) 
+       return num  
+    elif n== 1 :
+       print(nn) 
+       return nn
+    else:
+       return(sum_series(n-1 ,num , nn ) + sum_series(n-2 , num , nn)) 
 
 
 
