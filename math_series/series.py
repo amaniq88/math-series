6397d0803a9926b17c9178142d3c98d289cfb066
amaniq88 <amani.musallamq88@gmail.com>
2022-03-02 21:33:08 +0300

    All done

--- math_series/series.py
+++ math_series/series.py
@@ -1,4 +1,4 @@
-def FibRecursion():  
+def FibRecursion(n):  
     if n == 0:  
        return 0  
     elif n== 1 : 
@@ -8,7 +8,7 @@ def FibRecursion():
 
 
 
-def lucas():
+def lucas(n):
     if n == 0:  
        return 2  
     elif n== 1 : 
